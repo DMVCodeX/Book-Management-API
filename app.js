1ad725ce3012b37b6b6e0e2c1ecdbb2e01edaa0b
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-21 00:01:52 -0400

    create schema and migrate database

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-const express = require("express");
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to The Book Manager");
-});
-
-app.listen(8080, () => {
-  console.log("server is listening on port 8080");
-});
---
+++ prisma/migrations/20230521040033_book_manager_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `Books` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(300) NOT NULL,
+    `author` VARCHAR(50) NOT NULL,
+    `publicationYear` INTEGER NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

65bdbd4eef4ecb20f80b2a0e85180bf1c5efb8b5
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-22 01:09:57 -0400

    update schema to rename table books to book, data created successfully

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const { PrismaClient } = require("@prisma/client");
-const e = require("express");
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const allBooks = await prisma.books.findMany();
-  console.log(allBooks);
+  await prisma.book.create({
+    data: {
+      title: "Matilda",
+      author: "Roald Dahl",
+      publicationYear: 1988,
+      image: "https://m.media-amazon.com/images/I/81PUl2JIcDL._AC_UF1000,1000_QL80_.jpg",
+    },
+  });
+
+  const allBooks = await prisma.book.findMany({
+    where: {
+      title: "Matilda",
+    },
+  });
+  console.dir(allBooks, { depth: null });
 }
 
 main()

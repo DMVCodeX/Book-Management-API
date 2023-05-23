661f4beac96448482a3c6a1fe43ca16dd8d205d3
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-23 01:19:51 -0400

    CRUD with apollor/gql sucessful

---
+++ server.js
@@ -0,0 +1,56 @@
+import { ApolloServer, gql } from "apollo-server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const typeDefs = gql`
+  type Book {
+    id: Int
+    title: String
+    author: String
+    publicationYear: Int
+    image: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Query {
+    books: [Book]
+    book(id: Int!): Book
+  }
+
+  type Mutation {
+    createBook(title: String!, author: String!, publicationYear: Int!, image: String): Book
+    updateBook(id: Int!, title: String, author: String, publicationYear: Int, image: String): Book
+    deleteBook(id: Int!): Book
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => prisma.book.findMany(),
+    book: (_, { id }) => prisma.book.findUnique({ where: { id: parseInt(id) } }),
+  },
+
+  Mutation: {
+    createBook: (_, { title, author, publicationYear, image }) =>
+      prisma.book.create({ data: { title, author, publicationYear, image } }),
+    updateBook: (_, { id, title, author, publicationYear, image }) =>
+      prisma.book.update({ where: { id: parseInt(id) }, data: { title, author, publicationYear, image } }),
+    deleteBook: (_, { id }) => prisma.book.delete({ where: { id: parseInt(id) } }),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => ({ prisma }),
+});
+
+try {
+  server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
+} catch (error) {
+  console.error("Error starting the server:", error);
+}
--- src/server.js
+++
@@ -1,4 +0,0 @@
-const { ApolloServer, gql } = require("apollo-server");
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
